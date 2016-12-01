b5bf8d6ace96054bb2311ac3ce5bc67f40479bc2
dkingdon <daniel.kingdon@gmail.com>
2016-11-30 17:08:01 -0800

    sprint 5 complete. add, edit, and delete songs functioning.

--- public/scripts/controllers/AlbumsShowController.js
+++ public/scripts/controllers/AlbumsShowController.js
@@ -17,7 +17,6 @@ function AlbumsShowController ($http, $routeParams) {
     console.log('There was an error getting the data', response);
   });
 
-
   vm.deleteSong = function (song) {
     $http({
       method: 'DELETE',
@@ -30,4 +29,29 @@ function AlbumsShowController ($http, $routeParams) {
     });
   }
 
-}
+  vm.editSong = function (song) {
+    $http({
+      method: 'PUT',
+      url: '/api/albums/'+ $routeParams.id + '/songs/' + song._id ,
+      data: song
+    }).then(function successCallback(json) {
+      // don't need to do anything!
+    }, function errorCallback(response) {
+      console.log('There was an error editing the data', response);
+    });
+  }
+
+  vm.createSong = function () {
+    $http({
+      method: 'POST',
+      url: '/api/albums/' + $routeParams.id + '/songs',
+      data: vm.newSong
+    }).then(function successCallback(json) {
+      vm.album.songs.push(json.data);
+      vm.newSong = {};
+    }, function errorCallback(res) {
+      console.log('error creating new song', res);
+    });
+  }
+
+} // END OF CONTROLLER
